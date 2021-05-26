84762a88f194f56016d77e4838ef8a7131c24cc3
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-05-26 20:12:09 +0100

    fix: aumentar batch size

--- src/main/java/openpolitica/congreso/ProyectosLey2016.java
+++ src/main/java/openpolitica/congreso/ProyectosLey2016.java
@@ -10,7 +10,7 @@ public class ProyectosLey2016 {
         "http://www2.congreso.gob.pe",
         "/Sicr/TraDocEstProc/CLProLey2016.nsf/Local%20Por%20Numero?OpenView=&Start=",
         "/Sicr/TraDocEstProc/Expvirt_2011.nsf/visbusqptramdoc1621/%s?opendocument",
-        100);
+        500);
     var proyectos = ext.run();
     var avro = Path.of("data/proyectos-ley-2016.avro");
     var changed = ext.save(avro, proyectos);
